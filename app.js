d4225320ed85299c8b89ba72cd3fe535946d842a
Andrei <andreimandrique3@gmail.com>
2025-01-17 15:31:55 +0800

    add login page

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 3000;
 const path = require("path");
 
+const session = require("express-session");
+const passport = require("passport");
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
@@ -13,9 +19,11 @@ app.set("view engine", "ejs");
 
 const indexRouter = require("./routes/indexRouter");
 const signupRouter = require("./routes/signupRouter");
+const loginRouter = require("./routes/loginRouter");
 
 app.use("/", indexRouter);
 app.use("/sign-up", signupRouter);
+app.use("/log-in", loginRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
