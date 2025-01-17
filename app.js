b19fa58db357c8cf20dca36cea1a2a7af11dc96d
Andrei <andreimandrique3@gmail.com>
2025-01-18 01:03:31 +0800

    fix login

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const port = 3000;
 const path = require("path");
 
 const session = require("express-session");
-const passport = require("passport");
+const passport = require("./auth");
 
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
-app.use(passport.session());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
 
 const indexRouter = require("./routes/indexRouter");
 const signupRouter = require("./routes/signupRouter");
@@ -24,6 +24,13 @@ const loginRouter = require("./routes/loginRouter");
 app.use("/", indexRouter);
 app.use("/sign-up", signupRouter);
 app.use("/log-in", loginRouter);
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -4,5 +4,6 @@ const loginRouter = express.Router();
 const loginController = require("../controllers/loginController");
 
 loginRouter.get("/", loginController.loginGet);
+// loginRouter.post("/", loginController.loginPost);
 
 module.exports = loginRouter;
