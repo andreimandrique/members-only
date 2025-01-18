79c15598135658e695230215e318f647fe3fd4af
Andrei <andreimandrique3@gmail.com>
2025-01-18 14:57:33 +0800

    idk

--- routes/loginRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const loginRouter = express.Router();
-
-const loginController = require("../controllers/loginController");
-
-loginRouter.get("/", loginController.loginGet);
-
-module.exports = loginRouter;
